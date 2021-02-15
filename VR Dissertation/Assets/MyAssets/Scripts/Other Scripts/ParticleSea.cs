c49a08dda6b81776cb613f6fa47a59c8fc05a810
Michael Slape <46972251+Slapeyy@users.noreply.github.com>
2021-02-15 11:49:41 +0000

    Added particle sea
    Installed terrain height map package and created custom terrain from height map. Also downloaded island assets for terrain options, begun work on seaside level design. Created particle waves but still need to create speed variables.

---
+++ VR Dissertation/Assets/MyAssets/Scripts/Other Scripts/ParticleSea.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSea : MonoBehaviour
+{
+    public ParticleSystem particleSystem;               // creates public particle system variable
+    private ParticleSystem.Particle[] particlesArray;   // creates array for particles
+
+    public Gradient colourGradient;                     // Create a colour gradient for the particles
+
+    [Range(0, 250)]public int seaResolution = 25;       // creates value to handle resolution of the sea
+    public float spacing = 0.25f;                       // sets spacing between each particle
+    public float noiseScale = 0.2f;                     // change noise scale
+    public float heightScale = 3f;                      // change height scale
+
+    // Floats to change value with time for perlin noise function
+    private float perlinNoiseAnimX = 0.01f;
+    private float perlinNoiseAnimY = 0.01f;
+    public float xAnimationSpeed;
+    public float yAnimationSpeed;
+    public Vector2 perlinNoiseOffset;
+
+    public void XAnimationSpeed(float speed)
+    {
+        xAnimationSpeed = speed;
+    }
+
+    public void YAnimationSpeed(float speed)
+    {
+        yAnimationSpeed = speed;
+    }
+    private void Start()
+    {
+        // Set up new array with suitable space for all the particles
+        particlesArray = new ParticleSystem.Particle[seaResolution * seaResolution];
+
+        // Set max particle count = seaResolution squared
+        particleSystem.maxParticles = seaResolution * seaResolution;
+
+        // Spawn desired amount of particles
+        particleSystem.Emit(seaResolution * seaResolution);
+
+        // Assign spawned particles to the array
+        particleSystem.GetParticles(particlesArray);
+
+        StartCoroutine("Animate");
+    }
+
+    private void Update()
+    {
+        // Loop through all particles and place them based on their positions (seaRes = 25 particles in a row & every n-th 25 particles in the next row)
+        for (int i = 0; i < seaResolution; i++)
+        {
+            for (int j = 0; j < seaResolution; j++)
+            {
+                // Calculate zPos using Mathf.PerlinNoise and position of currently processed particle
+                float zPos = Mathf.PerlinNoise(i * noiseScale + perlinNoiseAnimX, j * noiseScale + perlinNoiseAnimY);
+
+                // Assign colour of particle by evaluating gradient by zPos variable
+                particlesArray[i * seaResolution + j].color = colourGradient.Evaluate(zPos);
+
+                particlesArray[i * seaResolution + j].position = new Vector3(i * spacing, zPos * heightScale, j * spacing);
+            }
+        }
+
+        // Add animations to X and Y coords
+        perlinNoiseAnimX += 0.01f;
+        perlinNoiseAnimY += 0.01f;
+
+        // Assign this array back to the particle system
+        particleSystem.SetParticles(particlesArray, particlesArray.Length);
+    }
+
+    IEnumerator Animate()
+    {
+        while (true)
+        {
+            perlinNoiseOffset = new Vector2(perlinNoiseOffset.x + xAnimationSpeed, perlinNoiseOffset.y + yAnimationSpeed);
+            yield return new WaitForSeconds(0.02f);
+        }
+    }
+}
---
+++ VR Dissertation/Assets/MyAssets/Scripts/Other Scripts/ParticleSea2.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ParticleSea2 : MonoBehaviour
+{
+	public Camera cam;
+	public bool render;
+	public ParticleSystem particleSystem;
+	public Vector3 meshOffset;
+	public int x_meshResolution;
+	public int y_meshResolution;
+	public float x_meshSpacing;
+	public float y_meshSpacing;
+	public float meshHeightScale;
+	public float perlinNoiseScale;
+	public Vector2 perlinNoiseOffset;
+	public Gradient colorsGradient;
+	public bool useGradientColors;
+	public Color defaultColor;
+	public float x_randomLimit;
+	public float y_randomLimit;
+
+	private Vector3[] positions;
+	private bool colorsAssigned = false;
+	private ParticleSystem.Particle[] particlesArray;
+	private float xPos;
+	private float yPos;
+	private float zPos;
+	public float xAnimSpeed;
+	public float yAnimSpeed;
+	private int frameCount;
+	private bool hasAnythingChanged;
+
+	public void PerlinNoiseScale(float noise)
+	{
+		if (noise > 0f)
+			perlinNoiseScale = noise * 100;
+	}
+
+	public void HeightModifier(float height)
+	{
+		if (height > 0f)
+			meshHeightScale = height * 3;
+	}
+
+	public void Spacing(float spacing)
+	{
+		hasAnythingChanged = true;
+		if (spacing > 0f)
+		{
+			x_meshSpacing = spacing;
+			y_meshSpacing = spacing;
+		}
+	}
+
+	public void Render(bool isActive)
+	{
+		render = isActive;
+	}
+
+	public void UseGradient(bool isActive)
+	{
+		useGradientColors = isActive;
+		colorsAssigned = false;
+	}
+
+	public void XAnimSpeed(float speed)
+	{
+		xAnimSpeed = speed;
+	}
+
+	public void YAnimSpeed(float speed)
+	{
+		yAnimSpeed = speed;
+	}
+
+	IEnumerator Animate()
+	{
+		while (true)
+		{
+			perlinNoiseOffset = new Vector2(perlinNoiseOffset.x + xAnimSpeed, perlinNoiseOffset.y + yAnimSpeed);
+			yield return new WaitForSeconds(0.02f);
+		}
+	}
+
+	public void Resolution(float i)
+	{
+		x_meshResolution = (int)i;
+		y_meshResolution = (int)i;
+		particlesArray = new ParticleSystem.Particle[x_meshResolution * y_meshResolution];
+		positions = new Vector3[x_meshResolution * y_meshResolution];
+		particleSystem.maxParticles = x_meshResolution * y_meshResolution;
+		hasAnythingChanged = true;
+
+	}
+
+	void Start()
+	{
+		particlesArray = new ParticleSystem.Particle[x_meshResolution * y_meshResolution];
+		positions = new Vector3[x_meshResolution * y_meshResolution];
+		StartCoroutine("Animate");
+		colorsAssigned = false;
+		hasAnythingChanged = true;
+	}
+
+	void Update()
+	{
+		if (render && frameCount % 2 == 0)
+		{
+			if (hasAnythingChanged) meshOffset = new Vector3(x_meshSpacing * x_meshResolution * -0.5f, y_meshSpacing * y_meshResolution * -0.5f, 0f);
+			hasAnythingChanged = false;
+			particleSystem.GetParticles(particlesArray);
+
+			for (int i = 0; i < x_meshResolution; i++)
+			{
+				for (int j = 0; j < y_meshResolution; j++)
+				{
+					xPos = (perlinNoiseOffset.x + i) / perlinNoiseScale;
+					yPos = (perlinNoiseOffset.y + j) / perlinNoiseScale;
+					zPos = Mathf.PerlinNoise(xPos, yPos);
+
+					//Set position based on these three
+					particlesArray[i * x_meshResolution + j].position = new Vector3(i * x_meshSpacing + meshOffset.x, j * y_meshSpacing + meshOffset.y, zPos * meshHeightScale);
+
+					if (useGradientColors) particlesArray[i * x_meshResolution + j].color = colorsGradient.Evaluate(zPos);
+					else if (!colorsAssigned) particlesArray[i * x_meshResolution + j].color = defaultColor;
+					colorsAssigned = true;
+				}
+			}
+			particleSystem.SetParticles(particlesArray, particlesArray.Length);
+		}
+		frameCount++;
+		colorsAssigned = true;
+	}
+}
